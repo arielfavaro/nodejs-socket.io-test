8c1f61ab5e8ceebcaa5ca6acf2ffcaca5ffc2027
Ariel Favaro <arieltainan@gmail.com>
2022-01-21 14:41:54 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
     socket.on('chat message', (message) => {
         console.log(`Message: ${message.message}`);
 
-        // io.emit('chat message feedback', message);
+        io.emit('chat message feedback', message);
     })
 
     socket.on('disconnect', () => {
